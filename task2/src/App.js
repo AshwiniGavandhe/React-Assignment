99afaaa7ad2802e2b33ba9591a8a904a21fd6e62
AshwiniGavandhe <109074602+AshwiniGavandhe@users.noreply.github.com>
2022-08-18 16:26:51 +0530

    Add files via upload
    task2

---
+++ task2/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './index.css';
+ import './App.css';
+
+import Registation from "./components/Registation";
+
+
+function App() {
+
+  return (
+    <div>
+   
+<Registation />
+
+      
+    </div>
+  )
+}
+
+export default App;
